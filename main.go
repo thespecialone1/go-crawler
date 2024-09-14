e4fae1601f5e7045e63a056424aa9c6b0b00bf82
thespecialone1 <s3neo36@gmail.com>
2024-09-14 20:52:35 +0400

    crawling blog posts from golangcode.com

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main()  {
+	blogTitles, err:= getLatestBlogTitles("https://golangcode.com")   //calling our blog title function (but we haven't written it yet)
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println("Blong Titles: ")
+	fmt.Printf(blogTitles)
+	
+}
+
+func getLatestBlogTitles(url string)(string, error)  {
+	resp, err:=http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	doc, err:= goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	titles:= ""
+	doc.Find(".post-title").Each(func (i int, s *goquery.Selection)  {
+		titles += "-" + s.Text() + "\n" 
+	})
+		return titles, nil
+}
